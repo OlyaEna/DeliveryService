cc48e0f65c100ed756f29c5638b416fc13c42230
olyao <olya.olka.ena@mail.ru>
2023-01-19 01:11:53 +0300

    flyway users

--- src/main/resources/db.migration/V2__Add_Prop.sql
+++ src/main/resources/db.migration/V2__Add_Prop.sql
@@ -13,6 +13,10 @@ insert into coffee_type values (3, 'N', 'Espresso','Эспрессо', 1.0 );
 insert into coffee_type values (4, 'N', 'Latte','Латте', 3.0 );
 insert into coffee_type values (5, 'N', 'Americano','Американо', 1.0 );
 
+# пароль - user
+insert into users values (12, 'user@user.ru','$2a$12$WiAuLPPJc9Z/aSsMZ8Vo7.jn1RapLVMcoXuUVSdpHWvxbs4YC6kBC');
+insert into users_roles values (12, 2);
 
-# insert into users values (3, 'inna13@mail.ru', ('user'));
-# insert into users_roles values (2, 2);
+# пароль - admin
+insert into users values (13, 'admin@admin.com','$2a$12$JZ2g4gw3CsvHxil0Si1WyOCEL6oFBX.pnDENxc9KRiwBoCJ8AmVNG');
+insert into users_roles values (13, 1);
