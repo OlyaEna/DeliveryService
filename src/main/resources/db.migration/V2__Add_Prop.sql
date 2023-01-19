becc2268eaa7997f20243c2708b2fbddea799b2a
olyao <olya.olka.ena@mail.ru>
2023-01-19 17:03:37 +0300

    V2_Add

--- src/main/resources/db.migration/V2__Add_Prop.sql
+++ src/main/resources/db.migration/V2__Add_Prop.sql
@@ -20,3 +20,26 @@ insert into users_roles values (12, 2);
 # пароль - admin
 insert into users values (13, 'admin@admin.com','$2a$12$JZ2g4gw3CsvHxil0Si1WyOCEL6oFBX.pnDENxc9KRiwBoCJ8AmVNG');
 insert into users_roles values (13, 1);
+
+# пароль - user
+insert into users values (14, 'user@gmail.com','$2a$12$HCVz.Usb.uaaxy.trCedEusSn4zgmT28ueW8EKVgnly798Qu6IqiG');
+insert into users_roles values (14, 2);
+
+insert into coffee_order values (4, 'Улица дорожная, 18', 25, 'Оля','2023-01-18 20:52:28.809218', 12);
+insert into coffee_order_item values (11, 5, 20, 4, 2);
+insert into coffee_order_item values (12, 1, 1, 4, 5 );
+insert into coffee_order_item values (13, 6, 6, 4, 3 );
+insert into coffee_order_item values (14, 1, 3, 4, 4 );
+
+insert into coffee_order values (5, 'Проспект Пушкина, 7, квартира 90', 11, 'Инна','2023-01-18 20:52:28.809218', 14);
+insert into coffee_order_item values (15, 5, 5, 5, 3 );
+insert into coffee_order_item values (16, 1, 3, 5, 4 );
+insert into coffee_order_item values (17, 1, 4, 5, 2 );
+
+insert into coffee_order values (6, 'Проспект Пушкина, 34', 11, 'User', '2023-01-19 16:59:21.940376', 12);
+insert into coffee_order_item values (18, 1, 4, 6, 2 );
+insert into coffee_order_item values (19, 1, 1, 6, 3 );
+insert into coffee_order_item values (20, 1, 3, 6, 4 );
+insert into coffee_order_item values (21, 1, 1, 6, 5 );
+
+
